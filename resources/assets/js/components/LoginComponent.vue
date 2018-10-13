a64ca375bf7d124ca39fb98a39c019fb7e506d15
rektph <kendrickjaviercosca@gmail.com>
2018-10-13 10:50:51 +0800

    Full width form

--- resources/assets/js/components/LoginComponent.vue
+++ resources/assets/js/components/LoginComponent.vue
@@ -6,12 +6,26 @@
                 <v-card>
                     <v-flex class="px-4 pb-4 pt2">    
                         <v-card-title primary-title>
-                        <div>
-                            <h3 class="headline mb-0">Login</h3>
-                            <v-divider></v-divider><br>
-                            Kiki
+                        <div class="full-width">
+                            <h3 class="headline mb-0">Login to <strong>POS</strong></h3>
+                            <v-divider></v-divider>
+                            <v-form v-model="valid">
+                                <v-text-field
+                                    class="pt-2"
+                                    v-model="email"
+                                    label="E-mail"
+                                    required />
+                                <v-text-field
+                                    class="pt-2 pb-2"
+                                    v-model="password"
+                                    :rules="errorRules"
+                                    label="Password"
+                                    type="password"
+                                    required />
+                            </v-form>
                         </div>
                         </v-card-title>
+
                         <v-card-actions>
                             <v-spacer/>
                             <v-btn color="primary" tabindex="1" :disabled="loading">Login</v-btn>
@@ -29,6 +43,18 @@ import { mapActions, mapGetters } from "vuex";
 
 export default {
     name: 'LoginComponent',
+    data: () => ({
+      valid: false,
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required'
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ]
+    }),
     methods: { },
     computed: mapGetters({
         loading: 'extras/loading'
