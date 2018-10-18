09137eae43165a8168b5d838057713ea23e61a11
rektph <kendrickjaviercosca@gmail.com>
2018-10-18 17:56:36 +0800

    Back-end Cleanup

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,38 +46,27 @@ class UserController extends Controller
                 'lname' => 'required|alpha',
                 'confpassword' => 'required|min:6|max:191|same:password'
             ]);
-            $names=array('email'=>"Email", 'password'=>"Password", 'fname'=>"First Name", 'lname'=>"Last Name", 'confpassword'=>"Confirm Password");
+            $names = array('email'=>"Email",
+                'password'=>"Password", 
+                'fname'=>"First Name", 
+                'lname'=>"Last Name", 
+                'confpassword'=>"Confirm Password");
+
             $validator->setAttributeNames($names);
-            if ($validator->fails()) {
-                return response()->json(["status"=>2,"errors"=>$validator->errors()]);
-            }
-            // $data = array("email"=>,
-            // "password"=>,        
-            // "name"=>());
+
+            if ($validator->fails()) return response()->json(["status"=>2,"errors"=>$validator->errors()]);
+
             $user = new User;
-            #print_r($request->input());
-            # print_r($data);
-            # $user->content = $data;123123
             $user->name = $request->input("fname") . ' ' . $request->input("lname");
             $user->email = $request->input("email");
             $user->password = password_hash($request->input("password"), PASSWORD_DEFAULT);
             $user->status = 1;
             $user->ipaddress = $request->ip();
-            #print_r($user);
             
             try {
-                if($user->save()) {
-                    return response()->json(["status"=>1]);
-                }
+                if($user->save()) return response()->json(["status"=>1]);
             } catch (\Exception $e) {
                 echo $e->getMessage();
-            }
-            
-            catch (QueryException $e){
-                $errorCode = $e->errorInfo[1];
-                if($errorCode == 1062){
-                    return response()->json(["status"=>1062]);
-                }
             }
         } else {
             return ['status' => 0, 'message' => "Wrong Method!"];
